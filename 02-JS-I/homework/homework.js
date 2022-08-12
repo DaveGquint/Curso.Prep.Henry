50322516af36b0f7a366dc843498eeaee9d34675
David A. Garcia Q <davgquint@gmail.com>
2022-08-12 17:51:01 -0300

    Second Commit JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -230,11 +230,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  var vocal = (letra.replace(/[^aeiouAEIOU]/g, " ").length);
-  if (vocal == 1) {return "Es vocal"
-  } else if (vocal != 1) {return "Dato incorrecto"
-    } else (letra.length > 1); {return "Dato incorrecto"}
+  
+if (letra === 'a' || letra === 'e' ||letra === 'i' ||letra === 'o' ||letra === 'u' ) {return "Es vocal"
 }
+if (letra !== 'a' || letra !== 'e' ||letra !== 'i' ||letra !== 'o' ||letra !== 'u' )  {return "Dato incorrecto"
+} 
+if (letra.length > 1); {return "Dato incorreto"
+    }
+}
+
 
 
 
